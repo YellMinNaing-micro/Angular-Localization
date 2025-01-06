f6bf0db759a1f5688115dfac894567382d6125a5
Yell Min Naing <yellminnaing5@gmail.com>
2025-01-06 13:47:34 +0630

    Update app.component.ts

--- test-localization/src/app/app.component.ts
+++ test-localization/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +14,38 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent implements OnInit {
 
   lang: string = '';
+  private appName = 'Test Localization';
 
-  constructor(private translateService: TranslateService) {
+  constructor(private translateService: TranslateService,
+    private titleService: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
   }
 
   ngOnInit(): void {
     this.lang = localStorage.getItem('lang') || 'en';
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          // Traverse the ActivatedRoute tree to get titles of nested routes
+          const titles: string[] = [];
+          while (route) {
+            if (route.snapshot.data['title']) {
+              titles.unshift(route.snapshot.data['title']);
+            }
+            route = route.firstChild!;
+          }
+          return titles;
+        })
+      )
+      .subscribe((titles) => {
+        // Join the titles and append the app name
+        const fullTitle = [...titles, this.appName].join(' | ');
+        this.titleService.setTitle(fullTitle);
+      });
   }
 
   changeLang(lang: any) {
