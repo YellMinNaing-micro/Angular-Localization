25d62d777a7c91b3c11412b0ccc2b1e98baa6fc5
Yell Min Naing <yellminnaing5@gmail.com>
2025-01-06 13:47:28 +0630

    Update app-routing.module.ts

--- test-localization/src/app/app-routing.module.ts
+++ test-localization/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    data: { title: 'Home' }
+  },
+  {
+    path: 'test',
+    component: TestComponent,
+    data: { title: 'Test' },
+
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], // Enable hash routing
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
